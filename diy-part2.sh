a02c9368581aa61c88d1b940a72dfff6a3797ded
acooler15 <renzongjia@vip.qq.com>
2020-04-29 20:13:42 +0800

    修改闪存为32M

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
