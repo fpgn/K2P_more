ff4c2a9dfb615c59b2f9e11ced02589080c06bc1
acooler15 <renzongjia@vip.qq.com>
2020-04-30 19:11:29 +0800

    移除添加rosy主题包源

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git rosywrt https://github.com/rosywrt/luci-theme-rosy' feeds.conf.default
