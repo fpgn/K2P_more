80e8b61666da79af5fb12c07ea02f30e1d2f72f0
acooler15 <renzongjia@vip.qq.com>
2020-07-11 20:07:00 +0800

    fix reboot faild

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
